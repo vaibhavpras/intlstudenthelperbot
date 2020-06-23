ed48f15d9fc3c4d8071f977faecd2c0b5c9c024a
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-23 22:08:26 +0530

    added alert delay function
    also fixed the bug when no nickname added

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import datetime
+from datetime import timedelta
+from datetime import datetime
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -13,11 +17,15 @@ with open('countries.txt', 'r') as f:
     countries_list = [line.strip() for line in f]
 
 
-temp_list = ["India", "China", "Pakistan"]
-
 countries_list = [item.lower() for item in countries_list]
 
+alerted_list = {}
 
+def checkKey(dict, key):       
+    if key in dict.keys(): 
+        return 1
+    else: 
+        return 0 
 
 @client.event
 async def on_ready():
@@ -26,21 +34,52 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    if not message.author.nick:
+        await message.channel.send(f"""{message.author.mention} please add a nickname which has your country and college name (if applicable).""")
+        return
+
     nickname  = (message.author.nick).lower()
 
     if (message.author.bot):
          return
 
+    
+    print(message.author.nick)    
+           
+
     #isCountryAdded = [ele for ele in countries_list if(ele in nickname)] 
     isCountryAdded = any(ele in nickname for ele in countries_list) 
 
-    print(nickname)
+    isAlerted = checkKey(alerted_list, message.author.nick)
 
-    print(isCountryAdded)
+    #print(isAlerted)
+    #print(nickname)
+    #print(isCountryAdded)
 
-    test = bool(isCountryAdded)
+    if isAlerted == 1:
+        date1 = alerted_list[message.author.nick]
+        date2 = datetime.now()
 
-    if not test:
-        await message.channel.send(f"""{message.author.mention} Please add your country to your nickname""")
+        difference = (date2 - date1).total_seconds()
+        
+        #print (difference)
+
+        inSeconds = difference
+        if inSeconds > 120 and not isCountryAdded:
+            await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
+            alerted_list.update( {message.author.nick : datetime.now()} )
+        elif inSeconds > 120 and isCountryAdded:
+            del alerted_list[message.author.nick] 
+            return
+        else:
+            return    
+
+    else:
+        if not isCountryAdded:
+            await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
+            alerted_list.update( {message.author.nick : datetime.now()} )
+            #print (alerted_list)
+    
     
 client.run(TOKEN)
