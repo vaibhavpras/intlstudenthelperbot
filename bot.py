f8ed8cf75cd54c15cf184a844a6b803c1c703754
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-23 16:33:33 +0530

    completed nickname correction function

--- bot.py
+++ bot.py
@@ -9,6 +9,19 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+with open('countries.txt', 'r') as f:
+    countries_list = [line.strip() for line in f]
+
+#countries_list = [countries]
+
+temp_list = ["India", "China", "Pakistan"]
+
+countries_list = [item.lower() for item in countries_list]
+
+print (countries_list)
+
+#print (countries_list)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -16,6 +29,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message.author.nick)
+    nickname  = (message.author.nick).lower()
 
+    #isCountryAdded = [ele for ele in countries_list if(ele in nickname)] 
+    isCountryAdded = any(ele in nickname for ele in countries_list) 
+
+    print(nickname)
+
+    print(isCountryAdded)
+
+    test = bool(isCountryAdded)
+
+    if not test:
+        await message.channel.send(f"""{message.author.mention} Please add your country to your nickname""")
+    
 client.run(TOKEN)
