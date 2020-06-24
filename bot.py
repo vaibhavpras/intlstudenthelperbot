16d6549fcea78601227f8475c06cb8fd810e7bda
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-24 13:23:34 +0530

    Added permission verification to !add_country
    nickname correction feature is completed

--- bot.py
+++ bot.py
@@ -10,12 +10,17 @@ from datetime import datetime
 
 from discord.ext.commands import Bot
 
+from discord.ext import commands
+
+from discord.ext.commands import has_permissions, MissingPermissions
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = Bot(command_prefix='!')
 
 alerted_list = {}
+
 
 def checkKey(dict, key):       
     if key in dict.keys(): 
@@ -29,31 +34,38 @@ async def on_ready():
     print(f'{bot.guilds}')
 
 
-@bot.command()
+@bot.command(name = "add_country", pass_context=True)
+@has_permissions(manage_roles=True)  
 async def add_country(ctx, arg):
     with open('countries.txt', 'a') as f:
         f.write(f"""\n{arg}""")
     await ctx.channel.send(f"""Added {arg} to the list of countries""")
 
+@add_country.error
+async def add_country_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
 @bot.event
 async def on_message(message):
 
+    
+    if (message.author.bot):
+        return
+
     if not message.author.nick:
         await message.channel.send(f"""{message.author.mention} please add a nickname which has your country and college name (if applicable).""")
+        await bot.process_commands(message)
         return
+    
+    nickname  = (message.author.nick).lower()
+    
+    print(message.author.nick)     
 
     with open('countries.txt', 'r') as f:
         countries_list = [line.strip() for line in f]
 
-
-    countries_list = [item.lower() for item in countries_list]
-
-    nickname  = (message.author.nick).lower()
-
-    if (message.author.bot):
-        return
-    
-    print(message.author.nick)            
+    countries_list = [item.lower() for item in countries_list]       
 
     #isCountryAdded = [ele for ele in countries_list if(ele in nickname)] 
     isCountryAdded = any(ele in nickname for ele in countries_list) 
