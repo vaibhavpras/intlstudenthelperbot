3482c3186234b59daba790d2170ac922f9166afd
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-26 12:34:01 +0530

    updated nickname feature & improved code readability
    added case when no nickname exists.
    
    moved nickname function to a different file.

--- bot.py
+++ bot.py
@@ -4,29 +4,17 @@ import os
 import discord
 from dotenv import load_dotenv
 
-import datetime
-from datetime import timedelta
-from datetime import datetime
-
 from discord.ext.commands import Bot
-
 from discord.ext import commands
-
 from discord.ext.commands import has_permissions, MissingPermissions
+
+import nicknamecheck
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = Bot(command_prefix='!')
 
-alerted_list = {}
-
-
-def checkKey(dict, key):       
-    if key in dict.keys(): 
-        return 1
-    else: 
-        return 0 
 
 @bot.event
 async def on_ready():
@@ -46,63 +34,14 @@ async def add_country_error(ctx, error):
     if isinstance(error, MissingPermissions):
         await ctx.send("You don't have permission to do that!")
 
+    
 @bot.event
 async def on_message(message):
-
-    
-    if (message.author.bot):
-        return
-
-    if not message.author.nick:
-        await message.channel.send(f"""{message.author.mention} please add a nickname which has your country and college name (if applicable).""")
-        await bot.process_commands(message)
-        return
-    
-    nickname  = (message.author.nick).lower()
-    
-    print(message.author.nick)     
-
-    with open('countries.txt', 'r') as f:
-        countries_list = [line.strip() for line in f]
-
-    countries_list = [item.lower() for item in countries_list]       
-
-    #isCountryAdded = [ele for ele in countries_list if(ele in nickname)] 
-    isCountryAdded = any(ele in nickname for ele in countries_list) 
-
-    isAlerted = checkKey(alerted_list, message.author.nick)
-
-    #print(isAlerted)
-    #print(nickname)
-    print(isCountryAdded)
-
-    if isAlerted == 1:
-        date1 = alerted_list[message.author.nick]
-        date2 = datetime.now()
-
-        difference = (date2 - date1).total_seconds()
-        
-        #print (difference)
-
-        inSeconds = difference
-        if inSeconds > 600 and not isCountryAdded:
-            await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
-            alerted_list.update( {message.author.nick : datetime.now()} )
-        elif inSeconds > 600 and isCountryAdded:
-            del alerted_list[message.author.nick] 
-            await bot.process_commands(message)
-            return
-        else:
-            await bot.process_commands(message)
-            return    
-
-    else:
-        if not isCountryAdded:
-            await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
-            alerted_list.update( {message.author.nick : datetime.now()} )
-            #print (alerted_list)
-
     await bot.process_commands(message)
+
+    await nicknamecheck.nicknameCheck(message)
+
+    
     
     
 bot.run(TOKEN)
---
+++ nicknamecheck.py
@@ -0,0 +1,90 @@
+import os
+
+import discord
+
+import datetime
+from datetime import timedelta
+from datetime import datetime
+
+alerted_list = {}
+
+def checkKey(dict, key):       
+    if key in dict.keys(): 
+        return 1
+    else: 
+        return 0 
+
+async def nicknameCheck(message):
+    if (message.author.bot):
+        return        
+  
+
+    if message.author.nick: 
+
+        nickname  = (message.author.nick).lower()   
+
+        with open('countries.txt', 'r') as f:
+            countries_list = [line.strip() for line in f]
+            countries_list = [item.lower() for item in countries_list] 
+
+        #isCountryAdded = [ele for ele in countries_list if(ele in nickname)] 
+        isCountryAdded = any(ele in nickname for ele in countries_list)
+
+        isAlerted = checkKey(alerted_list, message.author.nick)
+
+        print(f""""{nickname}" isAlerted: {isAlerted} & isCountryAdded: {isCountryAdded}""")
+    
+
+        if isAlerted == 1:
+            date1 = alerted_list[message.author.nick]
+            date2 = datetime.now()
+
+            difference = (date2 - date1).total_seconds()
+        
+            print (f""" Time since alerted: {difference}""")
+
+            inSeconds = difference
+            if inSeconds > 60 and not isCountryAdded:
+                await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
+                alerted_list.update( {message.author.nick : datetime.now()} )
+            elif inSeconds > 60 and isCountryAdded:
+                del alerted_list[message.author.nick] 
+                return
+            else:
+                return 
+
+        else:
+            if not isCountryAdded:
+                await message.channel.send(f"""{message.author.mention} please add your country to your nickname""")
+                alerted_list.update( {message.author.nick : datetime.now()} )
+                return
+
+    else: 
+        isAlertedNoNick = checkKey(alerted_list, message.author.name)
+        print(f""""{message.author.name}" isAlerted: {isAlertedNoNick}""")
+    
+
+        if isAlertedNoNick == 1:
+            date1 = alerted_list[message.author.name]
+            date2 = datetime.now()
+
+            difference = (date2 - date1).total_seconds()
+        
+            print (f"""Time since alerted: {difference}""")
+
+            inSeconds = difference
+            if inSeconds > 60 and not message.author.nick:
+                await message.channel.send(f"""{message.author.mention} please add a nickname which has your country and college name (if applicable).""")
+                alerted_list.update( {message.author.name : datetime.now()} )
+            elif inSeconds > 60 and message.author.nick:
+                del alerted_list[message.author.name]
+                nicknameCheck(message)             
+                return
+            else:
+                return    
+
+        else:
+            if not message.author.nick:
+                await message.channel.send(f"""{message.author.mention} please add a nickname which has your country and college name (if applicable).""")
+                alerted_list.update( {message.author.name : datetime.now()} )
+                return
