dfeaab24de99acf3a2fcf59b42852f32dd2e6241
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-26 17:09:22 +0530

    working add country command

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import discord
 from dotenv import load_dotenv
 
 from discord.ext.commands import Bot
-from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
 import nicknamecheck
@@ -17,6 +16,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = Bot(command_prefix='!')
 
 
+@bot.command(name = "add_country", pass_context=True)
+@has_permissions(manage_roles=True)  
+async def add_country(ctx, arg):
+    with open('countries.txt', 'a') as f:
+        f.write(f"""\n{arg}""")
+    await ctx.channel.send(f"""Added {arg} to the list of countries""")
+
+@add_country.error
+async def add_country_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -27,7 +38,6 @@ async def on_ready():
 async def on_message(message):
     await bot.process_commands(message)
 
-    await nicknamecheck.nicknameCheck(message)    
-    
+    await nicknamecheck.nicknameCheck(message)       
     
 bot.run(TOKEN)
--- rolereaction.py
+++
@@ -1,9 +0,0 @@
-import json
-import discord
-
-
-from discord.ext.commands import Bot
-from discord.ext.commands import has_permissions, MissingPermissions
-
-bot = Bot(command_prefix='!')
-
