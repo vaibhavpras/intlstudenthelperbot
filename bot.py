85347c8cee6834b0d4ba0bc8f9e836db42e79188
Vaibhav P <vaibav.prasanna@gmail.com>
2020-06-27 20:37:01 +0530

    beta faq build completed

--- bot.py
+++ bot.py
@@ -1,20 +1,50 @@
 # bot.py
 import os
+import nicknamecheck
+import similarity
+import corpus
 
 import discord
 from dotenv import load_dotenv
+
+from similarity import find_most_similar
+from corpus import CORPUS
+
+import asyncio
 
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
-import nicknamecheck
-import rolereaction
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = Bot(command_prefix='!')
+
+msg_id = 0
+roles_map = {}
+
+def isQuestion(text):
+    question_words = ['What', 'When', 'How', 'Where', 'Who', '?']
+    question_words = [item.lower() for item in question_words] 
+    found = any(ele in text.lower() for ele in question_words)
+    return(found)
+
+
+def answer_question(text):
+    question = text
+    answer = find_most_similar(question)
+
+    final_answer = ''
+    final_question = ''
+
+    if answer['score'] > 0.4:
+        print(answer['score'])
+        final_answer = answer['answer']
+        final_question = answer['question']
+
+    return final_answer, final_question  
+        
 
 @bot.event
 async def on_ready():
@@ -25,7 +55,109 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-
-    await nicknamecheck.nicknameCheck(message)       
     
-bot.run(TOKEN)
+    if (message.author.bot):
+        return     
+
+    await nicknamecheck.nicknameCheck(message)   
+
+    if(isQuestion(message.content)):
+        (answer_text, question_text) = answer_question(message.content)
+
+        if answer_text:
+            botmessage = await message.channel.send(f"""Do you want the answer to: {question_text} ?""")
+            
+            await botmessage.add_reaction('\N{THUMBS UP SIGN}')
+            await botmessage.add_reaction('\N{THUMBS DOWN SIGN}')
+
+            def checkUp(reaction, user):
+                return user == message.author and (str(reaction.emoji) == '\N{THUMBS UP SIGN}' or str(reaction.emoji) == '\N{THUMBS DOWN SIGN}')
+
+            try:
+                reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=checkUp)
+            except asyncio.TimeoutError:
+                await botmessage.delete()
+            else:
+                print(reaction.emoji)
+                if reaction.emoji == '\N{THUMBS UP SIGN}':
+                    await botmessage.delete()
+                    await message.channel.send(answer_text)
+
+                elif reaction.emoji == '\N{THUMBS DOWN SIGN}':
+                    await botmessage.delete()
+
+
+@bot.command(name = "add_country", pass_context=True)
+@has_permissions(manage_roles=True)  
+async def add_country(ctx, arg):
+    with open('countries.txt', 'a') as f:
+        f.write(f"""\n{arg}""")
+    print(f"""Added {arg} to the list of countries""")
+    await ctx.channel.send(f"""Added {arg} to the list of countries""")
+
+@add_country.error
+async def add_country_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
+
+@bot.command(name = "add_role_message", pass_context=True)
+@has_permissions(manage_roles=True)  
+async def add_role_msg(ctx, arg):
+    global msg_id
+    msg_id = arg 
+    print(f"""Assigned {arg} as role_message id""")
+    await ctx.channel.send(f"""Assigned {arg} as role_message id""")
+
+@add_role_msg.error
+async def add_role_msg(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
+@bot.command(name = "add_role_emoji", pass_context=True)
+@has_permissions(manage_roles=True)  
+async def add_role_emoji(ctx, arg1: discord.Emoji, arg2):
+    global roles_map
+    roles_map.update({arg1.name: arg2})  
+    print(f"""Assigned {arg1} to role: {arg2}""")
+    await ctx.channel.send(f"""Assigned {arg1} to role: {arg2}""")
+
+@add_role_emoji.error
+async def add_role_emoji_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You don't have permission to do that!")
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    if payload.message_id == int(msg_id):
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+
+        role = discord.utils.get(guild.roles, name = roles_map[payload.emoji.name])
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print("Role Added!")
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+
+    if payload.message_id == int(msg_id):
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+
+        role = discord.utils.get(guild.roles, name = roles_map[payload.emoji.name])
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            await member.remove_roles(role)
+            print("Role removed!")
+
+
+  
+bot.run(TOKEN) 
---
+++ corpus.py
@@ -0,0 +1,40 @@
+
+CORPUS = [
+    {
+        "Question": "Is SAT better or the ACT?",
+        "Answer": "What is ACT? How does it compare to SAT? \nhttps://www.princetonreview.com/college/act-information"
+    },
+    {
+        "Question": "How do I prepare for SAT?",
+        "Answer": "For Reading and Writing, Erica Meltzer’s books (“The Critical Reader”) are the gold standard whereas for Maths, CollegePanda and UWorld (if you’re willing to dish out money) are very popular in the SAT community. \nIf you want more resources for the SAT, the subreddit r/SAT is a great place to start. Tutoring is not necessary to get a good score; however, if self-learning is not your cup of tea, a tutor might help."
+    },
+    {
+        "Question": "How do I prepare for ACT?",
+        "Answer": "For Reading and Writing, Erica Meltzer’s books (“The Critical Reader”) are the gold standard whereas, for Maths, CollegePanda and UWorld (if you’re willing to dish out money) are very popular in the ACT community. \nIf you want more resources for the ACT, the subreddit r/ACT is a great place to start. Tutoring is not necessary to get a good score; however, if self-learning is not your cup of tea, a tutor might help."
+    },
+    {
+        "Question": "How do I convert my SAT score to ACT score and vice versa?",
+        "Answer": "This is the SAT-ACT concordance table which will allow you to covert scores. \nhttps://www.docdroid.net/itSwM15/act-sat-concordance-tables-pdf"
+    },
+    {
+        "Question": "What score do I need in SAT/ACT for a T20\Ivy?",
+        "Answer": "This question is hard to give an answer to. Admission to these universities/colleges is known to be highly holistic; this means your acceptance is decided by a multitude of things, one of which is your SAT score. Therefore, there is no one answer since for example, if your ECs are very strong, they tend to make up for weak SAT scores. If you’re applying to a T20 school, SAT scores are usually more important than if you’re applying to LACs since LACs as a whole have a more holistic process. \n\nAlthough if you are an international needing aid, it is recommended to have a SAT score above 1500 just to be on the safe side of things.  However, having a SAT score above 1500 by no means guarantees acceptance just as having a SAT score below 1500 by no means guarantees a rejection. It is however not advisable to have an SAT score that’s very low (<1400). Make sure your SAT scores are at least above the 50th percentile from the college’s average SAT data of previous years, and 75th percentile to be safe."
+    },
+    {
+        "Question": "Is my SAT/ACT score good enough for X university?",
+        "Answer": "A good way to gauge how competitive you are for X university is to check what percentile your standardized scores fall into. However, admission is not assured even if you’re in a university’s 99th percentile since US admissions are “holistic”."
+    },
+    {
+        "Question": "Which univiersities give full rides to international students?",
+        "Answer": "Here's a compilation of known Full Rides/Full Tuition scholarships/aid compiled by your benevolent CO2024 senpais.\nhttps://www.docdroid.net/XElAuAT/internationals-scholarship-masterlist-pdf"
+    },
+    {
+        "Question": "What is an EFC?",
+        "Answer": "EFC is estimated ..."
+    },
+    {
+        "Question": "What score do I need on the TOEFL",
+        "Answer": "Hey, a quick note on the TOEFL (and equivalents).  Most colleges only care that you can hit their minimum score.  These vary by college and range from Bowling Green at 59 to Oxford at 110 (out of 120). For some colleges, they like to see scores that are a bit above their stated minimum, but most will simply view this as a pass/fail criterion."
+    }
+]
+    
